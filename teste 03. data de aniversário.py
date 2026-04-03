c3651fcc92833089084528e3e11b5111f2213999
CaioSouza122 <caiosouzaj3k@gmail.com>
2026-04-03 14:06:51 -0300

    Adicionando calculo de idade

--- teste 03. data de aniversário.py
+++ teste 03. data de aniversário.py
@@ -3,3 +3,8 @@ mês = input('mês')
 ano = input('ano')
 print('Você nasceu no dia', dia, 'de', mês, 'de', ano, '.' 'Correto?')
             
+# Adicionando calculoo de idade
+from datetime import date
+ano_atual = date.today().year
+idade = ano_atual - int(ano)
+print('Você tem', idade, 'anos.')
